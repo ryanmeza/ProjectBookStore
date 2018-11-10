6dc8342d0764488143f3f28372fa6d3a97941cd3
Ryan <ryanmmeza01@gmail.com>
2018-11-10 14:37:09 -0500

    Setting up dashboard, about, and books components

--- bookstore-web/src/app/app-routing.module.ts
+++ bookstore-web/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BooksComponent } from './pages/books/books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path:'about',
+    component: AboutComponent
+  }, 
+  {
+    path: 'books',
+    component: BooksComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bookstore-web/src/app/app.module.ts
+++ bookstore-web/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BooksComponent } from './pages/books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AboutComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
