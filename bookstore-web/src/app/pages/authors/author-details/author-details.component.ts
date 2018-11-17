241fc833a2b843315e904ff104e1f195836e3dca
Ryan <ryanmmeza01@gmail.com>
2018-11-17 13:48:09 -0500

    author-detail display

--- bookstore-web/src/app/pages/authors/author-details/author-details.component.ts
+++ bookstore-web/src/app/pages/authors/author-details/author-details.component.ts
@@ -10,9 +10,26 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class AuthorDetailsComponent implements OnInit {
 
+  public authorDetailBooks: AuthorDetail[] = [];
   constructor(private authorService: AuthorService) { }
 
   ngOnInit() {
+    this.getAuthorBooks();
+  }
+
+  private getAuthorBooks() {
+    this.authorService.getAuthorBooks(1).subscribe(
+      (response: AuthorDetail[]) => {
+        this.authorDetailBooks = response;
+        console.log('Http Response', this.authorDetailBooks);
+
+      },
+      (error: HttpErrorResponse) => {
+        console.log('Error occurred', error)
+      },
+      () => {
+        console.log("We're done");
+      });
   }
 
 }
